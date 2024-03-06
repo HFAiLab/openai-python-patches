2dfe8c7b4f940aec93ccdeccc9f08438a75de015
panrz@deepseek.com <panrz@deepseek.com>
2024-03-06 16:54:51 +0800

    fix: handle conflict

--- update.sh
+++ update.sh
@@ -5,7 +5,9 @@ cd "$(dirname "$0")"
 if [ -d ./openai-python ]; then
   cd openai-python
   git checkout main
+  git stash
   git pull
+  git stash pop
   cd ..
 else
   git clone https://github.com/openai/openai-python.git
